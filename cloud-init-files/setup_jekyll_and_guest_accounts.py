c4a379fc0632cb19c95e4ce1f27c8b4e3dc0315d
Chris Geroux <chris.m.geroux@gmail.com>
2022-10-07 02:20:29 +0000

    Fixed download links for cloud_init_webserver
    Also added command line options for number of users and to optionally delete accounts

--- cloud-init-files/setup_jekyll_and_guest_accounts.py
+++ cloud-init-files/setup_jekyll_and_guest_accounts.py
@@ -1,11 +1,12 @@
 import passphrase_generator
 import subprocess
 import crypt
+import argparse
 
 def replaceStrInFile(strMatch,strReplace,fileName):
   """Replace all occurrences of strMatch with strReplace in file fileName
   """
-  
+
   file=open(fileName,mode='r')
   fileText=file.read()
   file.close()
@@ -14,33 +15,33 @@ def replaceStrInFile(strMatch,strReplace,fileName):
   file.write(fileText)
   file.close()
 def createUserWebDirectory(username):
-  
+
   webDirectory="/var/www/html/"+username
   subprocess.run(["mkdir",webDirectory])
   subprocess.run(["chown",username+":"+username,webDirectory])
 def deleteUserWebDirectory(username):
-  
+
   webDirectory="/var/www/html/"+username
   subprocess.run(["rm","-rf",webDirectory])
 def createGuestAccounts(numAccounts):
-  
+
   print("creating guest accounts")
   guest_account_passphrase=passphrase_generator.getRandom3WordPhrase()
   guest_account_passphrase_enc=crypt.crypt(guest_account_passphrase)
   print("  Guest accounts passphrase: \""+guest_account_passphrase+"\"")
   for i in range(numAccounts):
-    
+
     username="{}{:02d}".format("user",i+1)
     print("  "+username)
     subprocess.run(["useradd","-s","/bin/bash","-m","-p",
       guest_account_passphrase_enc,username])
-    
+
     createUserWebDirectory(username)
 def deletGuestAccounts(numAccounts):
-  
+
   print("deleting guest accounts")
   for i in range(numAccounts):
-    
+
     username="{}{:02d}".format("user",i+1)
     print("  "+username)
     subprocess.run(["deluser","--remove-home",username])
@@ -48,21 +49,30 @@ def deletGuestAccounts(numAccounts):
 def enablePasswordSSHAuthentication():
   '''NOTE: really should have fail2ban running before this function is called.
   '''
-  
+
   print("***WARNING: SHOULD HAVE FAIL2BAN RUNNING NOW***")
   print("enabling ssh password authentication")
   replaceStrInFile("PasswordAuthentication no","PasswordAuthentication yes",
     "/etc/ssh/sshd_config")
   subprocess.run(["service","ssh","restart"])
 def installJekyll():
-  
+
   subprocess.run(["gem","install","jekyll","bundler"])
 def main():
-  
+
+  parser = argparse.ArgumentParser(description='Setup Jekyll and create guest accounts')
+  parser.add_argument('numGuestAccounts', metavar='N', type=int,
+                      help='Number of guest accounts to create')
+  #parser.add_argument('--delete', action=argparse.BooleanOptionalAction,help="Delete accounts rather than create them")
+  parser.add_argument('--delete', action='store_true',default=False,help="Delete accounts rather than create them [account creation is default action]")
+  args = parser.parse_args()
+
   installJekyll()
   enablePasswordSSHAuthentication()
-  createGuestAccounts(1)
-  #deletGuestAccounts(1)
-  
+  if args.delete:
+    deletGuestAccounts(args.numGuestAccounts)
+  else:
+    createGuestAccounts(args.numGuestAccounts)
+
 if __name__=="__main__":
   main()
